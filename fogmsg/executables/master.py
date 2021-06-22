31b5d02031999e5fe8c1323fd42fecb6d6dcfe01
Moritz Ludolph <mail@moritzludolph.de>
2021-06-22 16:31:40 +0200

    :sparkles: add cli arguments

--- fogmsg/executables/master.py
+++ fogmsg/executables/master.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 from os import path
 
@@ -5,9 +6,22 @@ sys.path.append(path.join(path.dirname(__file__), "..", ".."))
 
 from fogmsg.components import Master  # noqa
 
-
 if __name__ == "__main__":
-    master = Master()
+    parser = argparse.ArgumentParser(description="fogmsg Node")
+    parser.add_argument(
+        "-i",
+        "--ip",
+        type=str,
+        help="address that the node will bind to",
+        default="0.0.0.0",
+    )
+
+    parser.add_argument(
+        "-p", "--port", type=int, help="port that the node will bind to", default=4000
+    )
+
+    args = parser.parse_args()
+    master = Master(hostname=args.ip, port=args.port)
     try:
         master.run()
     except KeyboardInterrupt:
--- fogmsg/executables/node.py
+++ fogmsg/executables/node.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 from os import path
 
@@ -7,8 +8,42 @@ from fogmsg.components import Node  # noqa
 from fogmsg.components.sensor import MockSensor  # noqa
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="fogmsg Node")
+    parser.add_argument(
+        "--master",
+        type=str,
+        help="hostname of the master",
+        default="tcp://localhost:4000",
+    )
+
+    parser.add_argument(
+        "-i",
+        "--ip",
+        type=str,
+        help="address that the node will bind to",
+        default="0.0.0.0",
+    )
+
+    parser.add_argument(
+        "-p", "--port", type=int, help="port that the node will bind to", default=4001
+    )
+
+    parser.add_argument(
+        "--advertised_listener",
+        type=str,
+        help="the advertisement listener of this node",
+        default="tcp://localhost:4001",
+    )
+
+    args = parser.parse_args()
+
     sensor = MockSensor()
-    node = Node(sensor=sensor)
+    node = Node(
+        sensor=sensor,
+        hostname=args.ip,
+        port=args.port,
+        advertised_hostname=args.advertised_listener,
+    )
     try:
         node.run()
     except KeyboardInterrupt:
