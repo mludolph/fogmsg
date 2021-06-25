ca04092e32fa355f0efc93e6d94a6e47d5e6876a
Moritz Ludolph <mail@moritzludolph.de>
2021-06-25 16:16:28 +0200

    :hammer: add simple shell script to start node

--- scripts/start_node.sh
+++ scripts/start_node.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-python fogmsg/executables/node.py
+MASTER_IP=
+EXTERNAL_IP=
+SENSOR=metrics
+
+python fogmsg/executables/sensor.py --type=${SENSOR} --pipe-file="/tmp/${SENSOR}data" --log-level="critical"
+
+python fogmsg/executables/node.py --master="tcp://${MASTER_IP}:4000" \
+                                  --advertised-listener="tcp://${EXTERNAL_IP}:4001" \
+                                  --pipe-files="/tmp/${SENSOR}data"
+
